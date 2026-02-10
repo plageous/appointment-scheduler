6d545e360b7afcbb28e5460e871a0984819809f8
KJ <122484032+plageous@users.noreply.github.com>
2026-02-10 14:17:00 -0800

    Added hello world to app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+const PORT = 3000;
+
+
+app.get('/', (req, res) => {
+    res.send(`Hello, World!`);
+});
+
+// start server, listen on PORT
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
